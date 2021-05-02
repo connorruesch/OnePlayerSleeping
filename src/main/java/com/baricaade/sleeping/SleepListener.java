67dbf5a1db80f17b72fc1b923e5f52f7645e7475
Connor <baricaade@gmail.com>
2021-05-01 17:46:38 -0700

    Fix imports

--- src/main/java/com/baricaade/sleeping/SleepListener.java
+++ src/main/java/com/baricaade/sleeping/SleepListener.java
@@ -3,7 +3,6 @@ package com.baricaade.sleeping;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
